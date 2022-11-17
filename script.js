51fd1b32547e49484ca0259d79b68f067d4c0c5a
mmarhalim <115380281+mmarhalim@users.noreply.github.com>
2022-11-17 14:34:59 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var element = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "Hello world!"
+);
+
+ReactDOM.render(element, document.getElementById('root'));
---
+++ src/script.js
@@ -0,0 +1,6 @@
+const element = <h1 className="greeting">Hello world!</h1>
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
